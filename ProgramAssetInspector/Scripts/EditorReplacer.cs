a7eafd344e8099d589778073462d0f9f77a0a292
Xytabich <xytabich@gmail.com>
2021-02-19 17:42:01 +0300

    scripts

---
+++ ProgramAssetInspector/Scripts/EditorReplacer.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using UnityEditor;
+using System;
+using System.Reflection;
+using VRC.Udon.ProgramSources;
+using System.Collections.Generic;
+
+namespace IL2UPP.Editor
+{
+	public static class EditorReplacer
+	{
+		[InitializeOnLoadMethod]
+		private static void Init()
+		{
+			var metType = typeof(CustomEditor).Assembly.GetType("UnityEditor.CustomEditorAttributes+MonoEditorType");
+			var attributesCollection = new TypeInfo(typeof(CustomEditor).Assembly.GetType("UnityEditor.CustomEditorAttributes"));
+			if(!attributesCollection.GetStaticField<bool>("s_Initialized"))
+			{
+				attributesCollection.GetStaticMethod<Type, bool, Type>("FindCustomEditorTypeByType")(null, false);
+			}
+			var types = attributesCollection.GetStaticField<IDictionary>("kSCustomEditors").value;
+			var editors = (IList)types[typeof(SerializedUdonProgramAsset)];
+			foreach(var editor in editors)
+			{
+				var info = new TypeInfo(metType, editor);
+				if(info.GetInstanceField<Type>("m_InspectorType") != typeof(ProgramAssetInspector))
+				{
+					var isFallback = info.GetInstanceField<bool>("m_IsFallback");
+					isFallback.value = true;
+				}
+			}
+		}
+
+		private struct FieldProp<T>
+		{
+			public T value
+			{
+				get => (T)field.GetValue(instance);
+				set
+				{
+					if(field.IsInitOnly) throw new InvalidOperationException("Cannot write to read-only field");
+					field.SetValue(instance, value);
+				}
+			}
+
+			private FieldInfo field;
+			private object instance;
+
+			public FieldProp(FieldInfo field, object instance = null)
+			{
+				this.field = field;
+				this.instance = instance;
+			}
+
+			public static implicit operator T(FieldProp<T> field)
+			{
+				return field.value;
+			}
+		}
+
+		private class TypeInfo
+		{
+			private Type type;
+			private object instance;
+
+			private Dictionary<string, object> instanceMembersCache = null;
+			private Dictionary<string, object> staticMembersCache = null;
+
+			public TypeInfo(Type type, object instance = null)
+			{
+				this.type = type;
+				this.instance = instance;
+			}
+
+			public FieldProp<T> GetInstanceField<T>(string name)
+			{
+				if(instanceMembersCache == null || !instanceMembersCache.TryGetValue(name, out var member))
+				{
+					if(instanceMembersCache == null) instanceMembersCache = new Dictionary<string, object>();
+
+					member = new FieldProp<T>(type.GetField(name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic), instance);
+					instanceMembersCache[name] = member;
+				}
+				return (FieldProp<T>)member;
+			}
+
+			public FieldProp<T> GetStaticField<T>(string name)
+			{
+				if(staticMembersCache == null || !staticMembersCache.TryGetValue(name, out var member))
+				{
+					if(staticMembersCache == null) staticMembersCache = new Dictionary<string, object>();
+
+					member = new FieldProp<T>(type.GetField(name, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic), null);
+					staticMembersCache[name] = member;
+				}
+				return (FieldProp<T>)member;
+			}
+
+			public Func<Ti0, Ti1, To> GetStaticMethod<Ti0, Ti1, To>(string name)
+			{
+				if(staticMembersCache == null || !staticMembersCache.TryGetValue(name, out var member))
+				{
+					if(staticMembersCache == null) staticMembersCache = new Dictionary<string, object>();
+
+					member = Delegate.CreateDelegate(typeof(Func<Ti0, Ti1, To>), type.GetMethod(name,
+						BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic, null,
+						new Type[] { typeof(Ti0), typeof(Ti1) }, null));
+					staticMembersCache[name] = member;
+				}
+				return (Func<Ti0, Ti1, To>)member;
+			}
+
+			public static TypeInfo CtorInstance(Type type)
+			{
+				var instance = type.GetConstructor(BindingFlags.Public | BindingFlags.NonPublic, null, Type.EmptyTypes, null).Invoke(new object[0]);
+				return new TypeInfo(type, instance);
+			}
+		}
+	}
+}
---
+++ ProgramAssetInspector/Scripts/ProgramAssetInspector.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using VRC.Udon.Common.Interfaces;
+using VRC.Udon.ProgramSources;
+using VRC.Udon.VM.Common;
+
+#if UDON_DEBUG
+using VRC.Udon.Serialization.OdinSerializer;
+#endif
+
+namespace IL2UPP.Editor
+{
+	[CustomEditor(typeof(SerializedUdonProgramAsset))]
+	public class ProgramAssetInspector : UnityEditor.Editor
+	{
+		private SerializedProperty _serializedProgramBytesStringSerializedProperty;
+		private SerializedProperty _serializationDataFormatSerializedProperty;
+
+		private bool showProgram = false;
+		private IUdonProgram program = null;
+		private string variablesAddresses;
+		private string variablesNames;
+		private string disassemblyAddresses;
+		private List<GUIContent> disassembly = new List<GUIContent>();
+
+		void OnEnable()
+		{
+			_serializedProgramBytesStringSerializedProperty = serializedObject.FindProperty("serializedProgramBytesString");
+			_serializationDataFormatSerializedProperty = serializedObject.FindProperty("serializationDataFormat");
+		}
+
+		public override void OnInspectorGUI()
+		{
+			if(showProgram != EditorGUILayout.Foldout(showProgram, "Show program"))
+			{
+				showProgram = !showProgram;
+				if(showProgram)
+				{
+					program = (target as SerializedUdonProgramAsset).RetrieveProgram();
+					var symbols = program.SymbolTable;
+
+					var cachedSb = new StringBuilder();
+					var cachedSb2 = new StringBuilder();
+
+					var heapDump = new List<(uint address, IStrongBox strongBoxedObject, Type objectType)>();
+					var heap = program.Heap;
+					heap.DumpHeapObjects(heapDump);
+					heapDump.Sort((a, b) => a.address.CompareTo(b.address));
+
+					for(var i = 0; i < heapDump.Count; i++)
+					{
+						var address = heapDump[i].address;
+						cachedSb.AppendFormat("0x{0:X8}\n", address);
+						cachedSb2.AppendLine(symbols.HasSymbolForAddress(address) ? symbols.GetSymbolFromAddress(address) : "[Unknown]");
+					}
+
+					variablesAddresses = cachedSb.ToString();
+					variablesNames = cachedSb2.ToString();
+
+					cachedSb2.Clear();
+					disassembly.Clear();
+					var publicMethods = program.EntryPoints;
+					var bytes = program.ByteCode;
+					uint index = 0;
+					while(index < bytes.Length)
+					{
+						if(publicMethods.TryGetSymbolFromAddress(index, out string name))
+						{
+							cachedSb2.AppendLine();
+							disassembly.Add(new GUIContent(string.Format("{0}:", name)));
+						}
+						uint address = index;
+						uint variableAddress;
+						OpCode op = (OpCode)UIntFromBytes(bytes, index);
+						index += 4;
+						cachedSb.Clear();
+						cachedSb.Append(op);
+						GUIContent content;
+						switch(op)
+						{
+							case OpCode.PUSH:
+								cachedSb.Append(", ");
+								variableAddress = UIntFromBytes(bytes, index);
+								cachedSb.AppendFormat("<i>{0}</i>", symbols.HasSymbolForAddress(variableAddress) ? symbols.GetSymbolFromAddress(variableAddress) : "[Unknown]");
+								index += 4;
+								content = new GUIContent(cachedSb.ToString(), string.Format("0x{0:X8}", variableAddress));
+								break;
+							case OpCode.EXTERN:
+								cachedSb.Append(", \"");
+								cachedSb.Append(heap.GetHeapVariable(UIntFromBytes(bytes, index)));
+								cachedSb.Append("\"");
+								index += 4;
+								content = new GUIContent(cachedSb.ToString());
+								break;
+							case OpCode.JUMP:
+							case OpCode.JUMP_IF_FALSE:
+								cachedSb.Append(", ");
+								cachedSb.AppendFormat("0x{0:X8}", UIntFromBytes(bytes, index));
+								index += 4;
+								content = new GUIContent(cachedSb.ToString());
+								break;
+							case OpCode.JUMP_INDIRECT:
+								cachedSb.Append(", ");
+								variableAddress = UIntFromBytes(bytes, index);
+								cachedSb.AppendFormat("<i>{0}</i>", symbols.HasSymbolForAddress(variableAddress) ? symbols.GetSymbolFromAddress(variableAddress) : "[Unknown]");
+								index += 4;
+								content = new GUIContent(cachedSb.ToString(), string.Format("0x{0:X8}", variableAddress));
+								break;
+							default:
+								content = new GUIContent(cachedSb.ToString());
+								break;
+						}
+						cachedSb2.AppendFormat("0x{0:X8}\n", address);
+						disassembly.Add(content);
+					}
+					disassemblyAddresses = cachedSb2.ToString();
+				}
+				else disassembly.Clear();
+			}
+			if(showProgram)
+			{
+				if(program != null)
+				{
+					GUILayout.Label("Variables:", EditorStyles.boldLabel);
+					EditorGUILayout.BeginHorizontal();
+					GUILayout.Label(variablesAddresses);
+					GUILayout.Label(variablesNames);
+					GUILayout.FlexibleSpace();
+					EditorGUILayout.EndHorizontal();
+
+					var style = new GUIStyle(EditorStyles.label);
+					style.border = RemoveVertical(style.border);
+					style.margin = RemoveVertical(style.margin);
+					style.padding = RemoveVertical(style.padding);
+					style.richText = true;
+					var height = style.lineHeight;
+					var heightProp = GUILayout.Height(height);
+
+					GUILayout.Label("Program:", EditorStyles.boldLabel);
+					EditorGUILayout.BeginHorizontal();
+					GUILayout.Label(disassemblyAddresses, style);
+					EditorGUILayout.BeginVertical();
+					for(var i = 0; i < disassembly.Count; i++)
+					{
+						GUILayout.Label(disassembly[i], style, heightProp);
+					}
+					EditorGUILayout.EndVertical();
+					GUILayout.FlexibleSpace();
+					EditorGUILayout.EndHorizontal();
+				}
+				else showProgram = false;
+			}
+
+#if UDON_DEBUG
+			DrawSerializationDebug();
+#endif
+		}
+
+		private RectOffset RemoveVertical(RectOffset ro)
+		{
+			ro.top = 0;
+			ro.bottom = 0;
+			return ro;
+		}
+
+#if UDON_DEBUG
+		private void DrawSerializationDebug()
+		{
+			EditorGUILayout.LabelField($"DataFormat: {(DataFormat)_serializationDataFormatSerializedProperty.enumValueIndex}");
+
+			if(string.IsNullOrEmpty(_serializedProgramBytesStringSerializedProperty.stringValue))
+			{
+				return;
+			}
+
+			if(_serializationDataFormatSerializedProperty.enumValueIndex == (int)DataFormat.JSON)
+			{
+				using(new EditorGUI.DisabledScope(true))
+				{
+					EditorGUILayout.TextArea(System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(_serializedProgramBytesStringSerializedProperty.stringValue)));
+				}
+			}
+			else
+			{
+				using(new EditorGUI.DisabledScope(true))
+				{
+					SerializedUdonProgramAsset serializedUdonProgramAsset = (SerializedUdonProgramAsset)target;
+					IUdonProgram udonProgram = serializedUdonProgramAsset.RetrieveProgram();
+					byte[] serializedBytes = SerializationUtility.SerializeValue(udonProgram, DataFormat.JSON, out List<UnityEngine.Object> _);
+					EditorGUILayout.TextArea(System.Text.Encoding.UTF8.GetString(serializedBytes));
+				}
+			}
+		}
+#endif
+
+		private static uint UIntFromBytes(byte[] bytes, uint startIndex)
+		{
+			return (uint)((bytes[startIndex] << 24) + (bytes[startIndex + 1] << 16) + (bytes[startIndex + 2] << 8) + bytes[startIndex + 3]);
+		}
+	}
+}
